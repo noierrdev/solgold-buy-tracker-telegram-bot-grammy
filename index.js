fca6acac9ec8d6fc842bc8505cd2f26f4748f648
Vander Moleker <noierdev@proton.me>
2024-05-16 06:26:15 +0200

    html

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ connection.onLogs(mintAddress,(data)=>{
             const amount=transaction.meta.postTokenBalances[1].uiTokenAmount.uiAmount-transaction.meta.preTokenBalances[1].uiTokenAmount.uiAmount
             const time=new Date(transaction.blockTime*1000);
             bot.api.sendMessage(chat,
-            `Buyer : 
+            `<b>Buyer :</b>
             <a href="https://explorer.solana.com/address/${receiver}?cluster=devnet">${receiver}</a>
-            Amount : 
-            ${amount}
-            Bought: 
-            ${time.toString()} 
-            Signature : 
-            <a href="https://explorer.solana.com/tx/${data.signature}?cluster=devnet">${data.signature}</a>
+            <b>Amount :</b>
+            <b>${amount}</b>
+            <b>Bought:</b>
+            <b>${time.toString()}</b>
+            <b>Signature :<b/>
+            <a href="https://explorer.solana.com/tx/${data.signature}?cluster=devnet">View on Explorer</a>
             `,{parse_mode:"HTML"})
             // bot.api.sendMessage(chat,messageContent.substring(4096,8191))
         });
